84ed95159a0dd06e1f7c42fbcce1097ca4d0121b
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-09-09 15:21:23 +0530

    import java.util.Scanner; class xyz {     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         if(n<0)         {             System.out.println("Negative Number");         }         else         {             System.out.println("Positive Number");         }         sc.close();     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class xyz
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n<0)
+        {
+            System.out.println("Negative Number");
+        }
+        else
+        {
+            System.out.println("Positive Number");
+        }
+        sc.close();
+    }
+}
