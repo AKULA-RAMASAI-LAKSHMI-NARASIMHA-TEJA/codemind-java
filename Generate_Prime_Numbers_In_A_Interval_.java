ff2fc54340eb7a961e05f3f4d595b260f5059e8e
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-09-27 10:30:44 +0530

    import java.util.Scanner; class valid {     public static boolean prime(int n)     {         if(n<2)         {             return false;         }         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             {                 return false;             }         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         for(int i=a;i<=b;i++)         {             if(prime(i))             {                 System.out.println(i);             }         }     } }

---
+++ Generate_Prime_Numbers_In_A_Interval_.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class valid
+{
+    public static boolean prime(int n)
+    {
+        if(n<2)
+        {
+            return false;
+        }
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=a;i<=b;i++)
+        {
+            if(prime(i))
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
