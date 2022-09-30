91290f480de42dee7259b85e1880102837e8c636
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-09-30 22:33:18 +0530

    import java.util.*; class pos {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),c=0;         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int k=sc.nextInt();         for(int i=0;i<n;i++)         {             if(a[i]==k)             {                 System.out.print(i+" ");                 c=1;                 break;             }         }         for(int i=n-1;i>=0;i--)         {             if(a[i]==k)             {                 System.out.print(i+" ");                 c=1;                 break;             }         }         if(c==0)         {             System.out.print("-1 -1");         }
        }
    }

---
+++ _Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class pos
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),c=0;
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==k)
+            {
+                System.out.print(i+" ");
+                c=1;
+                break;
+            }
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            if(a[i]==k)
+            {
+                System.out.print(i+" ");
+                c=1;
+                break;
+            }
+        }
+        if(c==0)
+        {
+            System.out.print("-1 -1");
+        }
+        
+    }
+}
