d24265d2bed009c63ef149cf528ec32cad5b96d7
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-10-01 14:05:43 +0530

    import java.util.*; class max {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int c=0;         for(int i=0;i<s.length();i++)         {             char ch=s.charAt(i);             if(Character.isDigit(ch))             {                 c++;             }         }         if(c!=0)         System.out.println("Contains "+c+" digit");         else         System.out.println("Doesn't contain digit");     } }

---
+++ string_contain_digit_or_not.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class max
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch))
+            {
+                c++;
+            }
+        }
+        if(c!=0)
+        System.out.println("Contains "+c+" digit");
+        else
+        System.out.println("Doesn't contain digit");
+    }
+}
