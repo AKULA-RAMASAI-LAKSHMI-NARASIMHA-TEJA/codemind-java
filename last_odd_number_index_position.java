90b7e967ce580f0243b5713907724dbd5b242459
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-10-07 21:02:51 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),i,ind=0;         int a[]=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]%2!=0)             {                 ind=i;             }         }         System.out.print(ind);     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),i,ind=0;
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2!=0)
+            {
+                ind=i;
+            }
+        }
+        System.out.print(ind);
+    }
+}
