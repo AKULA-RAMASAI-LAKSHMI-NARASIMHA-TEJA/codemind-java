31526bcff1582571307998476576fc43e9b8acb3
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-10-07 21:05:51 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),i,num=0;         int a[]=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]%2==0)             {                 num=a[i];             }         }         System.out.print(num);     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),i,num=0;
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2==0)
+            {
+                num=a[i];
+            }
+        }
+        System.out.print(num);
+    }
+}
