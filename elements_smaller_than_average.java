6f2b2424eff5dfee9c32a82380d1b3ff791323c5
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-10-07 20:43:55 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),s=0,i,avg,c=0;         int a[]=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             s+=a[i];         }         avg=s/n;         for(i=0;i<n;i++)         {             if(a[i]<=avg)             {                 c++;             }         }         System.out.println(c);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),s=0,i,avg,c=0;
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            s+=a[i];
+        }
+        avg=s/n;
+        for(i=0;i<n;i++)
+        {
+            if(a[i]<=avg)
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
