36d461042b75917f2953c77d6113cb2c0cfcb471
AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA <97424625+AKULA-RAMASAI-LAKSHMI-NARASIMHA-TEJA@users.noreply.github.com>
2022-10-07 21:52:27 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p=sc.nextInt(),t=sc.nextInt(),r=sc.nextInt();         System.out.println(p*t*r/100);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt(),t=sc.nextInt(),r=sc.nextInt();
+        System.out.println(p*t*r/100);
+    }
+}
